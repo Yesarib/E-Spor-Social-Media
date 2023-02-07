347a2ae222eca245597c6d91483a0110db1388b4
Yesarib <yesari_yesari@hotmail.com>
2023-02-07 23:09:13 +0300

    redux config

--- client/myreact/src/App.js
+++ client/myreact/src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Navbar from './component/Navbar/Navbar'
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Profile from './pages/Profile/Profile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/profile/:userId' element={<Profile />} />
         </Routes>
       </Router>
     </div>
---
+++ client/myreact/src/states/index.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    mode : 'light',
+    user: null,
+    token: null,
+    posts: [],
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers : {
+        setMode: (state) => {
+            state.mode = state.mode === 'light' ? 'dark' : 'light'; 
+        },
+        setLogin : (state,action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setFollow : (state,action) => {
+            if (state.user) {
+                state.user.follow = action.payload.follow;
+            } else {
+                console.error("User non exist")
+            }
+        },
+        setLogout: (state)=> {
+            state.user = null;
+            state.token = null;
+        },
+        setPosts: (state,action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state,action) => {
+            const updatedPost = state.posts.map((post )=> {
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPost;
+        }
+    }
+})
+
+export const { setMode,setLogin,setLogout,setFollow,setPost,setPosts } = authSlice.actions;
+
+export default authSlice.reducer;
