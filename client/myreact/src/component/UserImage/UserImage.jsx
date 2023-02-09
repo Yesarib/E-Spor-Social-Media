914fd37011b2a64a37c02f7f7c0d85f3c87a30ae
Yesarib <yesari_yesari@hotmail.com>
2023-02-10 00:35:44 +0300

    userimage and widget size

---
+++ client/myreact/src/component/UserImage/UserImage.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material';
+
+const UserImage = ({image,size='60px'}) =>  {
+    return (
+        <Box width={size} height={size}>
+            <img
+                style={{objectFit:'cover', borderRadius:'50%'}} 
+                alt='user'
+                width={size}
+                height={size}
+                src={`http://localhost:3001/assets/${image}`}
+            />
+        </Box>
+    )
+}
